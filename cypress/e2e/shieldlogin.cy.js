2b8112b1dde7817447b38522fca225a3d99e104d
Developer889 <124581162+Developer889@users.noreply.github.com>
2023-04-05 14:20:31 +0500

    Add files via upload

---
+++ cypress/e2e/shieldlogin.cy.js
@@ -0,0 +1,19 @@
+
+
+it('Login Test 1', () => {
+    cy.visit('http://127.0.0.1/');
+    cy.get('#name').type("ehtesham");
+    cy.get('#password').type("PYTHON");
+    cy.get('#dsds').click();
+    cy.get('#one').type("jyXQ4Q");
+    cy.get('.btn').click();
+})
+
+it('Login Test 2', () => {
+    cy.visit('http://127.0.0.1/');
+    cy.get('#name').type("admin");
+    cy.get('#password').type("gunie999D");
+    cy.get('#dsds').click();
+    cy.get('#one').type("jyXQ4Q");
+    cy.get('.btn').click();
+})
